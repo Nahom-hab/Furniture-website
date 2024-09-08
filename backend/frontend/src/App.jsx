582256e99f4a9f115e48abdc950d13cb2339f026
Nahom-hab <nahomhabtamu147@gmail.com>
2024-09-08 12:59:14 +0300

    commit

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Signup from './pages/Signup';
+// import Signup from './pages/Signup';
 import Home from './pages/home';
 import Shop from './pages/Shop';
 import Header from './components/Header';
@@ -16,8 +16,7 @@ export default function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/shop' element={<Shop />}></Route>
         <Route path='/product' element={<Product />}></Route>
-
-        <Route path='/signup' element={<Signup />}></Route>
+        {/* <Route path='/signup' element={<Signup />}></Route> */}
       </Routes>
     </BrowserRouter >
   )
